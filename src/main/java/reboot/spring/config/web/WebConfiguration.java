bce7c8a98b81f861450c4af0a8bf086ae39c9a83
yamdeng <yamdeng@gmail.com>
2021-07-24 20:31:09 +0900

    17.메시지소스 추가(파일)

--- src/main/java/reboot/spring/config/web/WebConfiguration.java
+++ src/main/java/reboot/spring/config/web/WebConfiguration.java
@@ -1,8 +1,12 @@
 package reboot.spring.config.web;
 
 
+import java.util.Locale;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @Configuration
 @ComponentScan("reboot.spring.config.web")
@@ -12,5 +16,15 @@ public class WebConfiguration {
 //    public HelloController helloController() {
 //        return new HelloController();
 //    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource =
+            new ResourceBundleMessageSource();
+        messageSource.setBasenames("message.server", "message.client");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setUseCodeAsDefaultMessage(true);
+        return messageSource;
+    }
 
 }
--- src/main/java/reboot/spring/config/web/controller/HelloController.java
+++ src/main/java/reboot/spring/config/web/controller/HelloController.java
@@ -1,5 +1,10 @@
 package reboot.spring.config.web.controller;
 
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +12,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Autowired
+    MessageSource messageSource;
+
     @GetMapping("/hello")
     @ResponseBody
     public String hello() {
         return "hello2";
+    }
+
+    @GetMapping("/message")
+    @ResponseBody
+    public Map<String, String> message() {
+        Map<String, String> resultMap = new HashMap<>();
+        String welcome = messageSource.getMessage("welcome", null, Locale.getDefault());
+        String hi = messageSource.getMessage("hi", null, Locale.getDefault());
+        String bye = messageSource.getMessage("bye", null, Locale.getDefault());
+        String ok = messageSource.getMessage("label.ok", null, Locale.getDefault());
+        String cancel = messageSource.getMessage("label.cancel", null, Locale.getDefault());
+        String buttonOk = messageSource.getMessage("button.ok", null, Locale.getDefault());
+        String buttonCancel = messageSource.getMessage("button.cancel", null, Locale.getDefault());
+        resultMap.put("welcome", welcome);
+        resultMap.put("hi", hi);
+        resultMap.put("bye", bye);
+        resultMap.put("ok", ok);
+        resultMap.put("cancel", cancel);
+        resultMap.put("buttonOk", buttonOk);
+        resultMap.put("buttonCancel", buttonCancel);
+        return resultMap;
     }
 
 }
