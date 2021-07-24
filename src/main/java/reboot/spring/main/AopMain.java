5ab985410799c42ff40fc036c545131dde5fdc5c
yamdeng <yamdeng@gmail.com>
2021-07-24 14:16:47 +0900

    proxyTargetClass proxy 적용 방식 확인

--- src/main/java/reboot/spring/main/AopMain.java
+++ src/main/java/reboot/spring/main/AopMain.java
@@ -7,12 +7,14 @@ import reboot.spring.bean.IFirstRepository;
 import reboot.spring.bean.IFirstService;
 import reboot.spring.bean.aop.ExceptionBean;
 import reboot.spring.config.AopConfig;
+import reboot.spring.config.AopConfigClass;
 
 public class AopMain {
 
     public static void main(String[] args) {
-        timecheckAop();
-        exceptionCheckAop();
+//        timecheckAop();
+//        exceptionCheckAop();
+        proxyTargetClass();
     }
 
     private static void timecheckAop() {
@@ -35,6 +37,21 @@ public class AopMain {
 //            e.printStackTrace();
         }
         ctx.close();
+    }
+
+    private static void proxyTargetClass() {
+        AnnotationConfigApplicationContext ctx =
+            new AnnotationConfigApplicationContext(AopConfig.class);
+        FirstDao firstDao1 = ctx.getBean("firstDao", FirstDao.class);
+        firstDao1.list();
+        System.out.println("firstDao1 class : " + firstDao1.getClass());
+
+        AnnotationConfigApplicationContext proxyTargetClassCtx =
+            new AnnotationConfigApplicationContext(AopConfigClass.class);
+
+        FirstDao firstDao2 = ctx.getBean("firstDao", FirstDao.class);
+        firstDao2.list();
+        System.out.println("firstDao2 class : " + firstDao2.getClass());
     }
 
 }
