98c86df8607ec91af549e37973b125444484bb4a
yamdeng <yamdeng@gmail.com>
2021-07-24 09:26:32 +0900

    2.junit 연동 테스트

--- src/test/java/reboot/spring/SpringFirstTest.java
+++ src/test/java/reboot/spring/SpringFirstTest.java
@@ -1,12 +1,27 @@
 package reboot.spring;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import reboot.spring.config.FirstSpringConfig;
 
 public class SpringFirstTest {
 
     @Test
     public void firstTest() {
-        System.out.println("first test");
+        AnnotationConfigApplicationContext ctx =
+            new AnnotationConfigApplicationContext(FirstSpringConfig.class);
+        FirstSpringConfig.FirstBean firstBean1 = ctx.getBean("firstBean", FirstSpringConfig.FirstBean.class);
+        FirstSpringConfig.FirstBean firstBean2 = ctx.getBean("firstBean", FirstSpringConfig.FirstBean.class);
+        System.out.println("firstBean1 : " + firstBean1);
+        System.out.println("firstBean2 : " + firstBean2);
+        boolean isBeanSame = false;
+        if(firstBean1 == firstBean2) {
+            isBeanSame = true;
+        }
+        System.out.println("isBeanSame : " + isBeanSame);
+        ctx.close();
+        assertEquals(true, isBeanSame);
     }
 
 }
