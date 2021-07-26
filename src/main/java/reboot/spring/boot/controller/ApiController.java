a46b905b37dff8b5946b035547aa7e85d798caf5
yamdeng <yamdeng@gmail.com>
2021-07-26 13:53:45 +0900

    12.rest api crud 추가

--- src/main/java/reboot/spring/boot/controller/ApiController.java
+++ src/main/java/reboot/spring/boot/controller/ApiController.java
@@ -2,8 +2,16 @@ package reboot.spring.boot.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reboot.spring.boot.vo.MemberVo;
@@ -13,18 +21,61 @@ import reboot.spring.boot.vo.MemberVo;
 @RequestMapping("/api")
 public class ApiController {
 
-    @GetMapping("/members")
-    public List<MemberVo> members() {
-        List<MemberVo> list = new ArrayList<>();
-        for (int index=0; index<10; index++) {
+    private List<MemberVo> list = new ArrayList<>();
+
+    {
+        list = new ArrayList<>();
+        for (long index=0; index<10; index++) {
             MemberVo memberVo = new MemberVo();
             memberVo.setEmail("yamdeng" + (index+1) + "@gmail.com");
             memberVo.setName("안용성23");
             memberVo.setLoginId("yamdeng" + (index+1));
+            memberVo.setId(index + 1);
             list.add(memberVo);
         }
+    }
+
+    @GetMapping("/members")
+    public List<MemberVo> members() {
         log.info("members size : " + list.size());
         return list;
+    }
+
+    @GetMapping("/members/{id}")
+    public MemberVo getDetail(@PathVariable Long id) {
+        Optional<MemberVo> optionalMemberVo = list.stream()
+            .filter(memberVo -> memberVo.getId().equals(id)).findAny();
+        return optionalMemberVo.get();
+    }
+
+    @PostMapping("/members")
+    public ResponseEntity<?> createMember(@RequestBody MemberVo memberVo) {
+        MemberVo lastMemberVo = list.get(list.size() - 1);
+        Long newId = lastMemberVo.getId() + 1;
+        memberVo.setId(newId);
+        list.add(memberVo);
+        return new ResponseEntity<>(
+            memberVo,
+            HttpStatus.OK);
+    }
+
+    @PutMapping("/members/{id}")
+    public MemberVo updateMember(@PathVariable Long id, @RequestBody MemberVo updateMemberVo) {
+        Optional<MemberVo> optionalMemberVo = list.stream()
+            .filter(memberVo -> memberVo.getId().equals(id)).findAny();
+        MemberVo originalMemberVo = optionalMemberVo.get();
+        originalMemberVo.setName(updateMemberVo.getName());
+        originalMemberVo.setEmail(updateMemberVo.getEmail());
+        originalMemberVo.setLoginId(updateMemberVo.getLoginId());
+        return originalMemberVo;
+    }
+
+    @DeleteMapping("/members/{id}")
+    public ResponseEntity<?> deleteMember(@PathVariable Long id) {
+        boolean removed = list.removeIf(memberVo -> memberVo.getId().equals(id));
+        return new ResponseEntity<>(
+            "delete id : " + id + ", success : " + removed,
+            HttpStatus.OK);
     }
 
 }
--- src/main/java/reboot/spring/boot/vo/MemberVo.java
+++ src/main/java/reboot/spring/boot/vo/MemberVo.java
@@ -9,6 +9,7 @@ import lombok.Data;
 @AllArgsConstructor
 public class MemberVo {
 
+    private Long id;
     private String name;
     private String loginId;
     private String email;
