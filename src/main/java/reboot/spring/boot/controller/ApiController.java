2d270552bf465dc595d04e988ca9a9ad8696c16a
yamdeng <yamdeng@gmail.com>
2021-07-26 09:54:49 +0900

    2.@RestController 추가

---
+++ src/main/java/reboot/spring/boot/controller/ApiController.java
@@ -0,0 +1,27 @@
+package reboot.spring.boot.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reboot.spring.boot.vo.MemberVo;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    @GetMapping("/members")
+    public List<MemberVo> members() {
+        List<MemberVo> list = new ArrayList<>();
+        for (int index=0; index<10; index++) {
+            MemberVo memberVo = new MemberVo();
+            memberVo.setEmail("yamdeng" + (index+1) + "@gmail.com");
+            memberVo.setName("안용성");
+            memberVo.setLoginId("yamdeng" + (index+1));
+            list.add(memberVo);
+        }
+        return list;
+    }
+
+}
---
+++ src/main/java/reboot/spring/boot/vo/MemberVo.java
@@ -0,0 +1,27 @@
+package reboot.spring.boot.vo;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MemberVo {
+
+    private String name;
+    private String loginId;
+    private String email;
+    private Boolean enabled;
+    private LocalDate regDate;
+    private LocalDate updateDate;
+    private LocalDateTime recentDateTime;
+
+    public MemberVo() {
+        this.regDate = LocalDate.now();
+        this.updateDate = LocalDate.now();
+        this.recentDateTime = LocalDateTime.now();
+        this.enabled = true;
+    }
+
+}
