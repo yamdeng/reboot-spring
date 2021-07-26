9c53148a61a437297165dda59c4c273368d338ca
yamdeng <yamdeng@gmail.com>
2021-07-26 10:48:18 +0900

    6.spring boot가 로드한 빈 이름 확인

--- src/main/java/reboot/spring/boot/BootApplication.java
+++ src/main/java/reboot/spring/boot/BootApplication.java
@@ -1,13 +1,23 @@
 package reboot.spring.boot;
 
+import java.util.Iterator;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
+@Slf4j
 @SpringBootApplication
 public class BootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(BootApplication.class, args);
+		Iterator<String> beanNameIterator = ctx.getBeanFactory().getBeanNamesIterator();
+		while(beanNameIterator.hasNext()) {
+			String beanName = beanNameIterator.next();
+			log.info("loaded beanName : " + beanName);
+		}
 	}
 
 }
