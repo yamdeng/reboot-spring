5c398ac08ddafcc4372107f323a2fdfc6cf108f9
yamdeng <yamdeng@gmail.com>
2021-07-26 11:29:56 +0900

    9.thymeleaf 추가

---
+++ src/main/java/reboot/spring/boot/controller/ThymeleafController.java
@@ -0,0 +1,16 @@
+package reboot.spring.boot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/view/th")
+public class ThymeleafController {
+
+    @GetMapping("/index")
+    public String indexPage() {
+        return "index";
+    }
+
+}
