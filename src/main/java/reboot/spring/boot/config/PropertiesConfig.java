cffc82e5526920136e12f9be5adae17060d091c7
yamdeng <yamdeng@gmail.com>
2021-07-29 11:07:12 +0900

    25.@PropertySource 반영 : application.properties 파일외에 다른 파일을 프로퍼티로 사용

--- src/main/java/reboot/spring/boot/config/PropertiesConfig.java
+++ src/main/java/reboot/spring/boot/config/PropertiesConfig.java
@@ -4,11 +4,13 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import reboot.spring.boot.properties.CustomProperties;
 import reboot.spring.boot.properties.YamdengProperties;
 
 @Configuration
 @EnableConfigurationProperties({YamdengProperties.class})
+@PropertySource(value={"classpath:custom.properties"})
 public class PropertiesConfig {
 
     @ConfigurationProperties(prefix = "custom")
--- src/main/java/reboot/spring/boot/controller/PropertiesController.java
+++ src/main/java/reboot/spring/boot/controller/PropertiesController.java
@@ -1,15 +1,25 @@
 package reboot.spring.boot.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reboot.spring.boot.properties.CustomProperties;
 import reboot.spring.boot.properties.YamdengProperties;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/properties")
 public class PropertiesController {
+
+    @Value("${spring.datasource.url}")
+    private String datasourceUrl;
+    @Value("${custom.prop1}")
+    private String customProp1;
 
     @Autowired
     private YamdengProperties yamdengProperties;
@@ -25,6 +35,14 @@ public class PropertiesController {
     @GetMapping("/custom")
     public CustomProperties customProperties() {
         return customProperties;
+    }
+
+    @GetMapping("/value")
+    public Map<String, Object> valueProObjectMap() {
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        resultMap.put("datasourceUrl", datasourceUrl);
+        resultMap.put("customProp1", customProp1);
+        return resultMap;
     }
 
 }
