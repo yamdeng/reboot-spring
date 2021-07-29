8108e98e178ca86ba48566de48126a04f5339411
yamdeng <yamdeng@gmail.com>
2021-07-29 15:02:12 +0900

    27-4.ConfigurationPropertiesScan auto scan

--- src/main/java/reboot/spring/boot/config/PropertiesConfig.java
+++ src/main/java/reboot/spring/boot/config/PropertiesConfig.java
@@ -1,15 +1,14 @@
 package reboot.spring.boot.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import reboot.spring.boot.properties.CustomProperties;
-import reboot.spring.boot.properties.YamdengProperties;
 
 @Configuration
-@EnableConfigurationProperties({YamdengProperties.class})
+@ConfigurationPropertiesScan({ "reboot.spring.boot.properties" })
 @PropertySource(value={"classpath:custom.properties", "classpath:platform.properties"})
 public class PropertiesConfig {
 
