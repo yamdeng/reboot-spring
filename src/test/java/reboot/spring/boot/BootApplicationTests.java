411d622e6e65c8273f1549843af746192bfd2d56
yamdeng <yamdeng@gmail.com>
2021-07-29 15:09:47 +0900

    27-5.@TestPropertySource 어노테이션 적용

--- src/test/java/reboot/spring/boot/BootApplicationTests.java
+++ src/test/java/reboot/spring/boot/BootApplicationTests.java
@@ -3,9 +3,11 @@ package reboot.spring.boot;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import reboot.spring.boot.util.HttpClientUtil;
 
 @SpringBootTest
+@TestPropertySource("classpath:/test.properties")
 class BootApplicationTests {
 
 	@Autowired
