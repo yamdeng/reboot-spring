30456166b0a333b552a0acefb4c4d7d3e870d42b
yamdeng <yamdeng@gmail.com>
2022-11-26 17:20:03 +0900

    mybatis setting

--- src/main/java/com/yamdeng/template/data/mapper/UserMapper.java
+++ src/main/java/com/yamdeng/template/data/mapper/UserMapper.java
@@ -8,6 +8,6 @@ import java.util.List;
 @Mapper
 public interface UserMapper {
 
-    List<UserVO> test() throws Exception;
+    List<UserVO> test();
 
 }
--- src/test/java/com/yamdeng/template/FirstApplicationTest.java
+++ src/test/java/com/yamdeng/template/FirstApplicationTest.java
@@ -1,15 +1,21 @@
 package com.yamdeng.template;
 
+import com.yamdeng.template.data.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest(classes = BootApplication.class)
+@SpringBootTest(classes = BootStandardApplication.class)
 class FirstApplicationTest {
+
+	@Autowired
+	private UserMapper userMapper;
 
 	@Test
 	void firstTest() {
         assertEquals("yamdeng", "yamdeng");
+		userMapper.test();
 	}
 
 }
