8dc7142031a3bdfe102437247483ed85956953be
yamdeng <yamdeng@gmail.com>
2022-12-10 19:30:47 +0900

    부서별_근무_시간_관리 fix

--- src/main/java/com/yamdeng/template/data/dao/DeptWorkTimeSettingDao.java
+++ src/main/java/com/yamdeng/template/data/dao/DeptWorkTimeSettingDao.java
@@ -1,13 +1,29 @@
 package com.yamdeng.template.data.dao;
 
+import com.yamdeng.template.vo.common.StatsCommonVO;
 import com.yamdeng.template.vo.db.OfficeDeptWorkTimeSettingVO;
 import com.yamdeng.template.vo.db.OfficePushAlarmVO;
+import com.yamdeng.template.vo.db.OfficeWorkReportVO;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface DeptWorkTimeSettingDao {
 
-    // 부서별 근무시간 셋팅 상세정보
+    // 부서별_근무_시간_관리 detail
     OfficeDeptWorkTimeSettingVO selectDeptWorkTimeSettingInfoByDeptId(String deptId);
+
+    // 부서별_근무_시간_관리 list
+    List<OfficeDeptWorkTimeSettingVO> selectDeptWorkTimeSettingList(OfficeDeptWorkTimeSettingVO vo);
+
+    // 부서별_근무_시간_관리 list totalCount
+    int selectDeptWorkTimeSettingListTotalCount(OfficeDeptWorkTimeSettingVO vo);
+
+    // 부서별_근무_시간_관리 insert
+    int insertDeptWorkTimeSetting(OfficeDeptWorkTimeSettingVO vo);
+
+    // 부서별_근무_시간_관리 update
+    int updateDeptWorkTimeSetting(OfficeDeptWorkTimeSettingVO vo);
 
 }
--- src/test/java/com/yamdeng/template/dao/DeptWorkTimeSettingDaoTest.java
+++ src/test/java/com/yamdeng/template/dao/DeptWorkTimeSettingDaoTest.java
@@ -11,6 +11,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @SpringBootTest(classes = BootStandardApplication.class)
 @Slf4j
 class DeptWorkTimeSettingDaoTest {
@@ -23,6 +25,59 @@ class DeptWorkTimeSettingDaoTest {
 	void selectDeptWorkTimeSettingInfoByDeptId() {
 		OfficeDeptWorkTimeSettingVO result = deptWorkTimeSettingDao.selectDeptWorkTimeSettingInfoByDeptId("dept1");
 		log.info("selectDeptWorkTimeSettingInfoByDeptId result : {}", result);
+	}
+
+	// 부서별_근무_시간_관리 list
+	@Test
+	void selectDeptWorkTimeSettingList() {
+		OfficeDeptWorkTimeSettingVO vo =
+			OfficeDeptWorkTimeSettingVO.builder()
+					.build();
+		List<OfficeDeptWorkTimeSettingVO> result = deptWorkTimeSettingDao.selectDeptWorkTimeSettingList(vo);
+		int totalCount = deptWorkTimeSettingDao.selectDeptWorkTimeSettingListTotalCount(vo);
+		log.info("selectDeptWorkTimeSettingList result : {}", result);
+		log.info("selectDeptWorkTimeSettingList totalCount : {}", totalCount);
+	}
+
+	// 부서별_근무_시간_관리 insert
+	@Test
+	void insertDeptWorkTimeSetting() {
+		OfficeDeptWorkTimeSettingVO vo =
+				OfficeDeptWorkTimeSettingVO.builder()
+						.deptId("dept1")
+						.timeName("시간명")
+						.timeDescription("설명")
+						.workLocation("근무지")
+						.workStartTime("12:00")
+						.workEndTime("18:00")
+						.lunchStartTime("12:00")
+						.lunchEndTime("13:00")
+						.applyStartDateStr("20221201")
+						.applyEndDateStr("")
+						.build();
+		int result = deptWorkTimeSettingDao.insertDeptWorkTimeSetting(vo);
+		log.info("insertDeptWorkTimeSetting result : {}", result);
+	}
+
+	// 부서별_근무_시간_관리 update
+	@Test
+	void updateDeptWorkTimeSetting() {
+		OfficeDeptWorkTimeSettingVO vo =
+				OfficeDeptWorkTimeSettingVO.builder()
+						.deptId("dept1")
+						.timeName("시간명2")
+						.timeDescription("설명2")
+						.workLocation("근무지2")
+						.workStartTime("12:01")
+						.workEndTime("18:01")
+						.lunchStartTime("12:01")
+						.lunchEndTime("13:01")
+						.applyStartDateStr("20221201")
+						.applyEndDateStr("")
+						.loginUserId("yamdeng")
+						.build();
+		int result = deptWorkTimeSettingDao.updateDeptWorkTimeSetting(vo);
+		log.info("updateDeptWorkTimeSetting result : {}", result);
 	}
 
 }
